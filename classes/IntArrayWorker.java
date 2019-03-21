8bd1c661891bf1ac37868cada9ee081f6d134b07
serlinecai4935 <scai4935@bths.edu>
2019-03-21 08:25:14 -0400

    did act 1-4

--- classes/IntArrayWorker.java
+++ classes/IntArrayWorker.java
@@ -27,6 +27,18 @@ public class IntArrayWorker
     }
     return total;
   }
+
+  public int getCount(int val) {
+    int count = 0;
+    for(int row = 0; row < matrix.length; row++) {
+      for(int col = 0; col < matrix[0].length; col++) {
+        if(matrix[row][col] == val) {
+          count++;
+        }
+      }
+    }
+    return count;
+  }
   
   /**
    * Method to return the total using a nested for-each loop
